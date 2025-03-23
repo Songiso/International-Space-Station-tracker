80f9c49a36f0c50b4315b8c4ed45b7e0d17eaa24
Songiso <songicooper@gmail.com>
2025-03-23 22:12:22 +0200

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -5,7 +5,6 @@ if __name__ == "__main__":
     print("Fetching ISS Position...")
     iss_position_data = iss_position()
     print("ISS Position Data Inserted:", iss_position_data)
-
     print("Fetching Astronauts in Space...")
     astronauts_data = astronauts_in_space()
     print("Astronauts Data Inserted:", astronauts_data)
